3a55b35a96a325804c97fd87b60e9122bac8b705
hightechtraders <hihgtech2@gmail.com>
2026-09-22 15:21:40 +0300

    Update index.tsx

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -9,6 +9,7 @@ import { crypto_currencies_display_order, fiat_currencies_display_order } from '
 import Footer from './footer';
 import AppHeader from './header';
 import Body from './main-body';
+import FloatingAI from '../../Aiscanner/FloatingAI';
 import './layout.scss';
 
 const Layout = observer(() => {
@@ -153,6 +154,7 @@ const Layout = observer(() => {
                 <Outlet />
             </Body>
             {!isCallbackPage && isDesktop && <Footer />}
+            <FloatingAI />
         </div>
     );
 });
